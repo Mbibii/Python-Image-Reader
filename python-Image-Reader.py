92e35658854705a2abc0a0f08f0e68881f58c3f6
Chris Turner <christopherturner3006@gmail.com>
2025-08-01 22:25:18 +0100

    Improving the function that reads the image file - it currently reads the file and outputs a 1 for black pixels and nothing for white. In the middle of making it read into a file - it replaces the top of the file each time - need to fix.

--- python-Image-Reader.py
+++ python-Image-Reader.py
@@ -1,35 +1,54 @@
 from PIL import Image
 import time
 
-fileLoc = ""
 fileLoc = input()
-
-pixelSize = 1
 
 img = ""
 
 # USE C:\Users\chris\source\repos\Python-Image-Reader\images\sprite_test.jpg FOR TESTING
 
+#FUNCTION TO OUTPUT TO FILE - DOESN'T WORK FULLY ATM
+def _writeToFile(file, text):
+    file.write(text)
+
+
 #FUNCTION TO READ PIXELS
-def _getPixels(w, h):
+def _getPixels(w, h, px):
+
     line = ""
+
     # loop for the width
-    for x in range(w):
+    for x in range(h):
+        line = line + "# "
         # loop for the height
-        for y in range(h):
-            line = line + ' (' + str(x) + ', ' + str(y) + ') '
+        for y in range(w):
+            
+            #line = line + ' (' + str(x) + ', ' + str(y) + ') '
+            if px[x, y] == (0, 0, 0):
+                line = line + " 1 "
+            else:
+                line = line + "   "
+
+        line = line + " #"
         print(line)
-        line = "" # resetting string
-        time.sleep(1)
+        
+        with open("C:\\Users\\chris\\source\\repos\\Python-Image-Reader\\files\\demofile.txt", "w") as filePath:
+            _writeToFile(filePath, line)
+
+        line = "" # resetting string    
+        time.sleep(0.1)
         # once line is finished it will go to next row
+
 
 #---------------------------------------------------------------------#
 
 #MAIN CODE
 img = Image.open(fileLoc)
 img = img.convert("RGB")
+
 #print(img)
 width, height = img.size
+px = img.load()
 
-_getPixels(width, height)
+_getPixels(width, height, px)
 
